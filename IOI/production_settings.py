a6538fa44a56397738372b364b0f6049443333e8
Mohammadreza MontazeriShatoori <monta2009@gmail.com>
2017-04-21 13:25:59 +0430

    Fix allowed hosts

--- IOI/production_settings.py
+++ IOI/production_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jq5w*u%s@o&b5gf59qmuby*y1$xrm5cpcw5kbxb86vu@z^1gm9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['trans.ioi2017.org']
 
 
 # Application definition

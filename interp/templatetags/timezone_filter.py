cd02a3c326f2496aa16bd0de595335e1102f41e7
Hamid <zarrabi@sharif.edu>
2017-05-26 00:33:30 +0430

    Edited timezone

--- interp/templatetags/timezone_filter.py
+++ interp/templatetags/timezone_filter.py
@@ -6,6 +6,6 @@ register = template.Library()
 @register.filter
 def ioi_timezone(value):
     tehran = pytz.timezone('Asia/Tehran')
-    fmt = '%b %d, %H:%M %Z'
+    fmt = '%b %d @ %H:%M'
 
     return value.astimezone(tehran).strftime(fmt)

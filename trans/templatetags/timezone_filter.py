9bdeedc72b5ea929f4fe755ea081071ba6fe163a
noid <navid.saleh.n@gmail.com>
2017-06-04 06:32:16 +0430

    Use settings timezone in timefilter

--- trans/templatetags/timezone_filter.py
+++ trans/templatetags/timezone_filter.py
@@ -1,11 +1,12 @@
 from django import template
+from django.conf import settings
 import pytz
 
 register = template.Library()
 
 @register.filter
 def ioi_timezone(value):
-    tehran = pytz.timezone('Asia/Tehran')
+    settings_timezone = pytz.timezone(settings.TIME_ZONE)
     fmt = '%b %d @ %H:%M'
 
-    return value.astimezone(tehran).strftime(fmt)
+    return value.astimezone(settings_timezone).strftime(fmt)

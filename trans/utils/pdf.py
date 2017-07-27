8cf60dd6aa536f95410ab42deebd3fb2eccd309d
noid <navid.saleh.n@gmail.com>
2017-07-27 19:26:41 +0430

    Route cpdf logs to dev null

--- trans/utils/pdf.py
+++ trans/utils/pdf.py
@@ -109,13 +109,13 @@ def convert_html_to_pdf(html, pdf_file_path):
 
 def add_page_numbers_to_pdf(pdf_file_path, task_name):
     cmd = ('cpdf -add-text "{0} (%Page of %EndPage)" -font "Arial" ' +
-          '-font-size 10 -bottomright .75in {1} -o {1}').format(task_name.capitalize(), pdf_file_path)
+          '-font-size 10 -bottomright .75in {1} -o {1} > /dev/null 2>&1').format(task_name.capitalize(), pdf_file_path)
     os.system(cmd)
 
 
 def add_info_line_to_pdf(pdf_file_path, info):
     output_pdf_path = '/tmp/{}.pdf'.format(str(uuid4()))
-    cmd = 'cpdf -add-text "{}" -font "Arial" -font-size 10 -bottomleft .75in {} -o {}'.format(
+    cmd = 'cpdf -add-text "{}" -font "Arial" -font-size 10 -bottomleft .75in {} -o {} > /dev/null 2>&1'.format(
         info, pdf_file_path, output_pdf_path)
     os.system(cmd)
     return output_pdf_path

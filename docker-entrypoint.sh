740a0ccd17fde6666d68666f39c168ea305f18b1
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-24 14:46:23 +0900

    allow executing custom command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,16 +5,22 @@ export DJANGO_SETTINGS_MODULE=Translation.settings
 
 fc-cache
 
-while ! timeout 2 bash -c "cat < /dev/null > /dev/tcp/$DB_HOST/5432" 2> /dev/null; do
-    echo "Waiting for db..."
-    sleep 1
-done
+if [[ -n $DB_HOST ]]; then
+    while ! timeout 2 bash -c "cat < /dev/null > /dev/tcp/$DB_HOST/5432" 2> /dev/null; do
+        echo "Waiting for db..."
+        sleep 1
+    done
+fi
 
-echo "Collecting staticfiles"
-python3 manage.py collectstatic --noinput
+if [[ $# -eq 0 ]]; then
+    echo "Collecting staticfiles"
+    python3 manage.py collectstatic --noinput
 
-echo "Migrating Models"
-python3 manage.py migrate
+    echo "Migrating Models"
+    python3 manage.py migrate
 
-echo "Starting Gunicorn"
-exec /usr/local/bin/gunicorn Translation.wsgi:application -w $GUNICORN_WORKERS -b :8000
+    echo "Starting Gunicorn"
+    exec /usr/local/bin/gunicorn Translation.wsgi:application -w $GUNICORN_WORKERS -b :8000
+fi
+
+exec "$@"

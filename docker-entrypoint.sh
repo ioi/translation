8528161082b931a6e49c82ccb20682a2ca128a53
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-24 10:59:13 +0900

    Revert "no shared volume"
    This reverts commit a2a841f7c4ecbf0a2948f57abd70d517ad42cd73.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,28 +5,16 @@ export DJANGO_SETTINGS_MODULE=Translation.settings
 
 fc-cache
 
+while ! timeout 2 bash -c "cat < /dev/null > /dev/tcp/$DB_HOST/5432" 2> /dev/null; do
+    echo "Waiting for db..."
+    sleep 1
+done
+
 echo "Collecting staticfiles"
 python3 manage.py collectstatic --noinput
 
-case "$1" in
-    nginx)
-        echo "Starting nginx"
-        exec /usr/sbin/nginx -g 'daemon off;'
-        ;;
+echo "Migrating Models"
+python3 manage.py migrate
 
-    gunicorn)
-        while ! timeout 2 bash -c "cat < /dev/null > /dev/tcp/$DB_HOST/5432" 2> /dev/null; do
-            echo "Waiting for db..."
-            sleep 1
-        done
-
-        echo "Migrating Models"
-        python3 manage.py migrate
-
-        echo "Starting Gunicorn"
-        exec /usr/local/bin/gunicorn Translation.wsgi:application -w $GUNICORN_WORKERS -b :8000
-        ;;
-
-    *)
-        exec "$@"
-esac
+echo "Starting Gunicorn"
+exec /usr/local/bin/gunicorn Translation.wsgi:application -w $GUNICORN_WORKERS -b :8000

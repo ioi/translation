3694af55c7668cd3881ffcebe456b3c11b2d2bce
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-31 18:59:24 +0900

    ioi2018 logos

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -2,8 +2,11 @@ from django.conf import settings
 
 def ioi_settings(request):
     return {'settings': {
-        'SITE_TITLE': 'IOI 2017 Translation',
-        'CONTEST_TITLE': 'IOI 2017',
+        'SITE_TITLE': 'IOI 2018 Translation',
+        'CONTEST_TITLE': 'IOI 2018',
+        'CONTEST_FULL_TITLE': 'International Olympiad in Informatics 2018',
+        'CONTEST_DATE': 'September 2\u20137th, 2018',
+        'CONTEST_PLACE': 'Tsukuba, Japan',
         'PRINT_ENABLED': True,
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': settings.HOST_URL + 'media/images/',

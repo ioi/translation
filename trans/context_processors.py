be6a5e78e06d5b12a8837c65766c6c9d735c8233
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-31 19:07:18 +0900

    configure printing via environment varaibles

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -7,7 +7,7 @@ def ioi_settings(request):
         'CONTEST_FULL_TITLE': 'International Olympiad in Informatics 2018',
         'CONTEST_DATE': 'September 2\u20137th, 2018',
         'CONTEST_PLACE': 'Tsukuba, Japan',
-        'PRINT_ENABLED': True,
+        'PRINT_ENABLED': settings.PRINT_SYSTEM_ADDRESS is not None,
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': settings.HOST_URL + 'media/images/',
         'HOST_URL': settings.HOST_URL

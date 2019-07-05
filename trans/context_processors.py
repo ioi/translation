b5bc6ae8e06f919161a8a33e357b0b8e45685cfc
Emil Abbasov <emil916@gmail.com>
2019-07-05 18:54:08 +0000

    Changed docker-compose.yml to work with up-to-date packages Set the timezone to Asia/Baku (in Translation/settings.py) Changed port to 9000 in docker-compose.yml, Translation/settings.py, docker-entrypoint.sh and nginx/docker-entrypoint.sh Uploaded ioi-2019 logos Edited login page to fir new logos Edited PDF generator page to show logo_text.svg on top left

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -2,11 +2,11 @@ from django.conf import settings
 
 def ioi_settings(request):
     return {'settings': {
-        'SITE_TITLE': 'IOI 2018 Translation',
-        'CONTEST_TITLE': 'IOI 2018',
-        'CONTEST_FULL_TITLE': 'International Olympiad in Informatics 2018',
-        'CONTEST_DATE': 'September 2\u20137th, 2018',
-        'CONTEST_PLACE': 'Tsukuba, Japan',
+        'SITE_TITLE': 'IOI 2019 Translation',
+        'CONTEST_TITLE': 'IOI 2019',
+        'CONTEST_FULL_TITLE': 'International Olympiad in Informatics 2019',
+        'CONTEST_DATE': 'August 4\u201311th, 2019',
+        'CONTEST_PLACE': 'Baku, Azerbaijan',
         'PRINT_ENABLED': settings.PRINT_SYSTEM_ADDRESS is not None,
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': '/media/images/',

8769f6d9fb8ea5bca78a4142653304ed817ad43c
Muhammad Rais Fathin Mudzakir <raisfathin@users.noreply.github.com>
2022-06-05 16:51:55 +0800

    Create skeleton and initial models for print job queue
    * Initialize print-job-queue app
    * Create models for print jobs
    * Create migrations for print_job_queue's initial model
    * Ignore emacs config files in git

---
+++ print_job_queue/models.py
@@ -0,0 +1,77 @@
+from enum import Enum, unique
+
+from django.conf import settings
+from django.db import models
+
+
+@unique
+class PrintJobState(Enum):
+    # The job has been created and is ready for pickup.
+    PENDING = 0
+
+    # The job is being processed by a worker.
+    IN_PROGRESS = 1
+
+    # The job has been processed by a worker. This is a terminal state.
+    DONE = 2
+
+    # The job has been invalidated. This is a terminal state.
+    INVALID = 3
+
+
+class PrintJob(models.Model):
+    STATE = (
+        (member.value, name)
+        for name, member in PrintJobState.__members__.items()
+    )
+    
+    job_id = models.AutoField(primary_key=True)
+
+    # See PrintJobState.
+    state = models.IntegerField(choices=STATE)
+
+    # The worker that is handling this job. Should be set when the job is in
+    # either PROCESSING or DONE state.
+    worker = models.CharField(max_length=25, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class PrintedDocument(models.Model):
+    # Path to the document to print.
+    file_path = models.FilePathField(match='*.pdf', recursive=True)
+
+    # The number of times the document should be printed. Should be greater
+    # than zero.
+    print_count = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class DraftPrintJob(PrintJob):
+    """Print job for draft translations.
+    
+    This is a separate table so job_id can be reused to distribute draft
+    printing jobs in a round-robin fashion.
+    """
+    pass
+
+
+class PrintedDraftDocument(PrintedDocument):
+    # The print job that contains this document.
+    job = models.ForeignKey(DraftPrintJob, on_delete=models.CASCADE)
+
+
+class FinalPrintJob(PrintJob):
+    """Print job for final translations.
+    
+    This is a separate table for the same reason DraftPrintJob is.
+    """
+    pass
+
+
+class PrintedFinalDocument(PrintedDocument):
+    # The print job that contains this document.
+    job = models.ForeignKey(FinalPrintJob, on_delete=models.CASCADE)

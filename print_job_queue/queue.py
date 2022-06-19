a74f4f1ab7b35a4a7734a0ebb69db0ef48f50a8b
Muhammad Rais Fathin Mudzakir <raisfathin@google.com>
2022-06-19 11:16:25 +0800

    Add print job handler for UserContest frozen bit change

--- print_job_queue/queue.py
+++ print_job_queue/queue.py
@@ -14,3 +14,25 @@ def enqueue_draft_print_job(file_path, print_count, owner):
 def query_all_draft_print_jobs():
     return list(
         models.DraftPrintJob.objects.all().prefetch_related('document_set'))
+
+
+def enqueue_final_print_job(file_paths_with_counts, owner):
+    job = models.FinalPrintJob.make_pending(owner=owner)
+    job.save()
+    for file_path, count in file_paths_with_counts.items():
+        doc = models.PrintedFinalDocument(job=job,
+                                          file_path=file_path,
+                                          print_count=count)
+        doc.save()
+    return job
+
+
+def query_all_final_print_jobs():
+    return list(
+        models.FinalPrintJob.objects.all().prefetch_related('document_set'))
+
+
+def invalidate_print_job(job):
+    # TODO: What if the print job is already done?
+    job.state = models.PrintJobState.INVALID.value
+    job.save()

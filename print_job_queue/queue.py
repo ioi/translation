f2ea4557f2a23c4a19366b6feda2e46428daa427
Muhammad Rais Fathin Mudzakir <raisfathin@google.com>
2022-06-21 01:14:13 +0800

    Implement business logic to mark draft jobs as done

--- print_job_queue/queue.py
+++ print_job_queue/queue.py
@@ -35,6 +35,29 @@ def pick_up_draft_job(job_id, worker_name):
     return True
 
 
+def mark_draft_job_complete(job_id, worker_name):
+    job = models.DraftPrintJob.objects.filter(job_id=job_id).first()
+    if job is None:
+        logger.warning(
+            'worker `%s` attempted to complete non-existent job with id %s',
+            worker_name, job_id)
+        return False
+    if job.state != models.PrintJobState.IN_PROGRESS.value:
+        logger.warning(
+            'worker `%s` attempted to complete non-in-progress job with id %s',
+            worker_name, job_id)
+        return False
+    if job.worker != worker_name:
+        logger.warning(
+            'worker `%s` attempted to complete another worker\'s (`%s`) job with id %s',
+            worker_name, job.worker, job_id)
+        return False
+
+    job.state = models.PrintJobState.DONE.value
+    job.save()
+    return True
+
+
 def enqueue_final_print_job(file_paths_with_counts, owner):
     job = models.FinalPrintJob.make_pending(owner=owner)
     job.save()

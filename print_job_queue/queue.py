bf73aa0e91514049aa689456c5633afaefa0b458
Muhammad Rais Fathin Mudzakir <raisfathin@google.com>
2022-06-21 01:02:58 +0800

    Implement business logic to pick up draft jobs

--- print_job_queue/queue.py
+++ print_job_queue/queue.py
@@ -16,6 +16,25 @@ def query_all_draft_print_jobs():
         models.DraftPrintJob.objects.all().prefetch_related('document_set'))
 
 
+def pick_up_draft_job(job_id, worker_name):
+    job = models.DraftPrintJob.objects.filter(job_id=job_id).first()
+    if job is None:
+        logger.warning(
+            'worker `%s` attempted to pick up non-existent job with id %s',
+            worker_name, job_id)
+        return False
+    if job.state != models.PrintJobState.PENDING.value:
+        logger.warning(
+            'worker `%s` attempted to pick up non-pending job with id %s',
+            worker_name, job_id)
+        return False
+
+    job.state = models.PrintJobState.IN_PROGRESS.value
+    job.worker = worker_name
+    job.save()
+    return True
+
+
 def enqueue_final_print_job(file_paths_with_counts, owner):
     job = models.FinalPrintJob.make_pending(owner=owner)
     job.save()

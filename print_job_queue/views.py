1a26a417bc443c762bf1b0b4aafdac69f3993b2d
Muhammad Rais Fathin Mudzakir <raisfathin@google.com>
2022-06-21 22:33:13 +0800

    Use template to show final print job queue
    Also sort the jobs by job_id for deterministic order in the UI

--- print_job_queue/views.py
+++ print_job_queue/views.py
@@ -38,6 +38,30 @@ def draft_queue(request):
         })
 
 
+def final_queue(request):
+    jobs = defaultdict(list)
+    for job in queue.query_all_final_print_jobs():
+        jobs[job.state].append({
+            'id':
+                job.job_id,
+            'owner':
+                job.owner.username,
+            'documents': [(document.file_path, document.print_count)
+                          for document in job.document_set.all()],
+        })
+
+    logger.info('jobs = %s', jobs)
+
+    return render(
+        request,
+        'final_queue.html',
+        context={
+            'in_progress_jobs': jobs[models.PrintJobState.IN_PROGRESS.value],
+            'pending_jobs': jobs[models.PrintJobState.PENDING.value],
+            'completed_jobs': jobs[models.PrintJobState.DONE.value],
+        })
+
+
 class DraftJobPickUp(View):
 
     def post(self, request, job_id):
@@ -66,26 +90,3 @@ class DraftJobMarkCompletion(View):
                 'Could not mark job as complete. Check log for more details.')
 
         return redirect(reverse('draft_queue'))
-
-
-def final_queue(request):
-    jobs = queue.query_all_final_print_jobs()
-
-    jobs_links = []
-    for job in jobs:
-        job_links = []
-        for document in job.document_set.all():
-            job_links.append((document.file_path, document.print_count))
-        jobs_links.append((job_links, job.owner.username, job.state))
-
-    # TODO(raisfathin): Use template.
-    link_html = lambda link: f'<a href="/{link[0]}">{link[0]} x {link[1]}</a>'
-    links_html = lambda links: '\n'.join(
-        [f'<li>{link_html(link)}</li>' for link in links])
-    job_links_html = lambda job: f'<ul>{links_html(job)}</ul>'
-    jobs_links_html = '\n'.join([
-        f'<li>Owner: {owner} ({models.PrintJobState(state)}) {job_links_html(job_links)}</li>'
-        for (job_links, owner, state) in jobs_links
-    ])
-
-    return HttpResponse(f'<ul>{jobs_links_html}</ul>')

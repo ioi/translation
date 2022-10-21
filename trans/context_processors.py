48467fac95e866560fe9091109d8c01de731a772
Ashar Fuadi <fushar@gmail.com>
2022-10-21 21:41:51 +0700

    Remove unused contest logo and titles

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -4,9 +4,6 @@ def ioi_settings(request):
     return {'settings': {
         'SITE_TITLE': 'IOI 2022 Task Translation System',
         'CONTEST_TITLE': 'IOI 2022',
-        'CONTEST_FULL_TITLE': 'International Olympiad in Informatics 2022',
-        'CONTEST_DATE': 'August 7\u201315th, 2022',
-        'CONTEST_PLACE': 'Yogyakarta, Indonesia',
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': '/media/images/',
     }}

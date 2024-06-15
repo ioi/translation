afa7e872bb540962ecaacb4b3aad5045bf70f42e
Martin Mareš <mj@ucw.cz>
2024-06-15 21:00:42 +0200

    Simplify URL routing to use path instead of re_path

--- print_job_queue/urls.py
+++ print_job_queue/urls.py
@@ -1,25 +1,17 @@
 from django.conf import settings
 
-from django.urls import re_path
+from django.urls import path
 from .views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
     # Draft job routes.
-    re_path(r'^draft/(?P<group>[\w]*)/$', DraftJobQueue.as_view(), name='draft_queue'),
-    re_path(r'^draft_job_pick_up/(?P<job_id>[\w]*)/$',
-        DraftJobPickUp.as_view(),
-        name='draft_job_pick_up'),
-    re_path(r'^draft_job_mark_completion/(?P<job_id>[\w]*)/$',
-        DraftJobMarkCompletion.as_view(),
-        name='draft_job_mark_completion'),
+    path('draft/<str:group>/', DraftJobQueue.as_view(), name='draft_queue'),
+    path('draft_job_pick_up/<str:job_id>/', DraftJobPickUp.as_view(), name='draft_job_pick_up'),
+    path('draft_job_mark_completion/<str:job_id>/', DraftJobMarkCompletion.as_view(), name='draft_job_mark_completion'),
 
     # Final job routes.
-    re_path(r'^final/(?P<group>[\w]*)/$', FinalJobQueue.as_view(), name='final_queue'),
-    re_path(r'^final_job_pick_up/(?P<job_id>[\w]*)/$',
-        FinalJobPickUp.as_view(),
-        name='final_job_pick_up'),
-    re_path(r'^final_job_mark_completion/(?P<job_id>[\w]*)/$',
-        FinalJobMarkCompletion.as_view(),
-        name='final_job_mark_completion'),
+    path('final/<str:group>/', FinalJobQueue.as_view(), name='final_queue'),
+    path('final_job_pick_up/<str:job_id>/', FinalJobPickUp.as_view(), name='final_job_pick_up'),
+    path('final_job_mark_completion/<str:job_id>/', FinalJobMarkCompletion.as_view(), name='final_job_mark_completion'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

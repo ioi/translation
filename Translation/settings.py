cd0ff990760f465cd24b9013b59e8516bf3930d5
Martin Mareš as root <mj@ucw.cz>
2024-07-01 17:40:25 +0200

    Django upgrade: A new name of the DB engine

--- Translation/settings.py
+++ Translation/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'Translation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],

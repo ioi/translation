c13873586c60686d5f76c3ae112f855410dc1580
Martin Mares <mj@ucw.cz>
2024-07-21 15:50:07 +0200

    Print refactor: Admin

--- print_job_queue/admin.py
+++ print_job_queue/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from print_job_queue.models import PrintJob, PrintedDocument, Worker
+
+admin.site.register(PrintJob)
+admin.site.register(PrintedDocument, list_display=['id', 'file_path', 'print_count'])
+admin.site.register(Worker, ordering=['name'])

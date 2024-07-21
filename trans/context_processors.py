23ab370bfc2825df1bde4ae258dbbc1f52c21eb9
Martin Mares <mj@ucw.cz>
2024-07-21 16:17:53 +0200

    Add context processor that provides is_editor to all templates

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 
+
 def ioi_settings(request):
     return {'settings': {
         'SITE_TITLE': 'IOI Task Translation System',
@@ -7,3 +8,15 @@ def ioi_settings(request):
         'TIME_ZONE': settings.TIME_ZONE,
         'IMAGES_URL': '/media/images/',
     }}
+
+
+def ioi_user(request):
+    user = request.user
+    if user is None:
+        is_editor = False
+    else:
+        is_editor = user.is_staff and user.groups.filter(name='editor').exists() or user.is_superuser
+
+    return {
+        'is_editor': is_editor,
+    }

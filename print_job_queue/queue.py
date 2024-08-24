98fc2e5e20a9e768e85260c696d6fc79b3121135
Martin Mares <mj@ucw.cz>
2024-08-24 21:52:11 +0200

    Print refactor: Queue management

--- print_job_queue/queue.py
+++ print_job_queue/queue.py
@@ -7,99 +7,38 @@ from print_job_queue import models
 logger = logging.getLogger(__name__)
 
 
-def enqueue_draft_print_job(file_path, print_count, owner, owner_country,
-                            group):
-    job = models.DraftPrintJob.make_pending(owner=owner,
-                                            owner_country=owner_country,
-                                            group=group)
+def enqueue_draft_print_job(file_path, print_count, group, owner):
+    job = models.PrintJob(state=models.PrintJobState.PENDING.value,
+                          group=group,
+                          job_type=models.PrintJobType.DRAFT.value,
+                          owner=owner)
     job.save()
-    doc = models.PrintedDraftDocument(job=job,
-                                      file_path=file_path,
-                                      print_count=print_count)
+    logger.info(f'Job #{job.id}: Enqueued draft job')
+
+    doc = models.PrintedDocument(job=job,
+                                 file_path=file_path,
+                                 print_count=print_count)
     doc.save()
+    logger.info(f'Job #{job.id}: Added document {doc.file_path} copies={doc.print_count}')
+
     return job
 
 
-def query_worker_print_jobs(print_job_model_cls, group, worker_name, worker_mod,
-                            worker_count):
-    assert worker_name != '', 'Blank string is the default value used in the db.'
-
-    # Fake comments so yapf can format the following statement nicely :(
-    return list(
-        print_job_model_cls.objects  #
-        .filter(group=group)  #
-        .prefetch_related('document_set')  #
-        .annotate(job_mod=F('job_id') % worker_count)  #
-        .filter(
-            # Either a job that this worker has already claimed, completed job,
-            # or an unclaimed job that is in the worker's queue based on the
-            # worker's mod.
-            Q(worker=worker_name) | Q(state=models.PrintJobState.DONE.value) |
-            (Q(worker='') & Q(job_mod=worker_mod)))  #
-        .order_by('job_id'))
-
-
-def query_group_print_jobs(print_job_model_cls, group):
-    # Fake comments so yapf can format the following statement nicely :(
-    return list(print_job_model_cls.objects  #
-                .filter(group=group)  #
-                .prefetch_related('document_set')  #
-                .order_by('job_id'))
-
-
-def pick_up_print_job(print_job_model_cls, job_id, worker_name):
-    job = print_job_model_cls.objects.filter(job_id=job_id).first()
-    if job is None:
-        logger.warning(
-            'worker `%s` attempted to pick up non-existent job with id %s',
-            worker_name, job_id)
-        return False
-    if job.state != models.PrintJobState.PENDING.value:
-        logger.warning(
-            'worker `%s` attempted to pick up non-pending job with id %s',
-            worker_name, job_id)
-        return False
-
-    job.state = models.PrintJobState.IN_PROGRESS.value
-    job.worker = worker_name
+def enqueue_final_print_job(file_paths_with_counts, group, owner):
+    job = models.PrintJob(state=models.PrintJobState.PENDING.value,
+                          group=group,
+                          job_type=models.PrintJobType.FINAL.value,
+                          owner=owner)
     job.save()
-    return True
+    logger.info(f'Job #{job.id}: Enqueued final job')
 
-
-def mark_print_job_complete(print_job_model_cls, job_id, worker_name):
-    job = print_job_model_cls.objects.filter(job_id=job_id).first()
-    if job is None:
-        logger.warning(
-            'worker `%s` attempted to complete non-existent job with id %s',
-            worker_name, job_id)
-        return False
-    if job.state != models.PrintJobState.IN_PROGRESS.value:
-        logger.warning(
-            'worker `%s` attempted to complete non-in-progress job with id %s',
-            worker_name, job_id)
-        return False
-    if job.worker != worker_name:
-        logger.warning(
-            'worker `%s` attempted to complete another worker\'s (`%s`) job with id %s',
-            worker_name, job.worker, job_id)
-        return False
-
-    job.state = models.PrintJobState.DONE.value
-    job.save()
-    return True
-
-
-def enqueue_final_print_job(file_paths_with_counts, owner, owner_country,
-                            group):
-    job = models.FinalPrintJob.make_pending(owner=owner,
-                                            owner_country=owner_country,
-                                            group=group)
-    job.save()
     for file_path, count in file_paths_with_counts.items():
-        doc = models.PrintedFinalDocument(job=job,
-                                          file_path=file_path,
-                                          print_count=count)
+        doc = models.PrintedDocument(job=job,
+                                     file_path=file_path,
+                                     print_count=count)
         doc.save()
+        logger.info(f'Job #{job.id}: Added document {doc.file_path} copies={doc.print_count}')
+
     return job
 
 
@@ -107,3 +46,84 @@ def invalidate_print_job(job):
     # TODO: What if the print job is already done?
     job.state = models.PrintJobState.INVALID.value
     job.save()
+    logger.info(f'Job #{job.id}: Invalidated')
+
+
+def query_group_print_jobs(group, job_type):
+    return list(models.PrintJob.objects
+                .filter(group=group, job_type=job_type.value)
+                .prefetch_related('document_set')
+                .order_by('id'))
+
+
+def query_worker_print_jobs(group, job_type, worker):
+    query = (models.PrintJob.objects
+             .filter(job_type=job_type.value, group=group))
+
+    if worker.job_type is not None:
+        query = query.filter(job_type=worker.job_type)
+
+    if worker.modulo > 0:
+        query = (query
+                 .annotate(id_mod=F('owner_id') % worker.modulo)
+                 .filter(Q(worker=worker) | Q(worker=None, id_mod=worker.index)))
+
+    return list(
+        query
+        .prefetch_related('document_set')
+        .order_by('id'))
+
+
+def pick_up_print_job(job_id, worker, do_print=False):
+    job = models.PrintJob.objects.filter(id=job_id).first()
+    if job is None:
+        logger.warning(f'Worker {worker.name}: Attempted to pick-up non-existent job #{job_id}')
+        return None
+    if job.state != models.PrintJobState.PENDING.value:
+        logger.warning(f'Worker {worker.name}: Attempted to pick-up non-pending job #{job_id}')
+        return None
+
+    if do_print:
+        job.state = models.PrintJobState.PRINTING.value
+    else:
+        job.state = models.PrintJobState.IN_PROGRESS.value
+    job.worker = worker
+    job.save()
+
+    # FIXME: Implement printing
+
+    logger.info(f'Worker {worker.name}: Job {job.id} {"started printing" if do_print else "picked up"}')
+    return job
+
+
+def mark_print_job_complete(job_id, worker):
+    job = models.PrintJob.objects.filter(id=job_id).first()
+    if job is None:
+        logger.warning(f'Worker {worker.name}: Attempted to complete non-existent job #{job_id}')
+        return False
+    if job.state not in (models.PrintJobState.IN_PROGRESS.value, models.PrintJobState.PRINTING.value):
+        logger.warning(f'Worker {worker.name}: Attempted to complete non-in-progress job #{job_id}')
+        return False
+    if job.worker != worker:
+        logger.warning(f"Worker {worker.name}: Attempted to complete another worker's job #{job_id}")
+        return False
+
+    job.state = models.PrintJobState.DONE.value
+    job.save()
+
+    logger.info(f'Worker {worker.name}: Job {job.id} completed')
+    return True
+
+
+def restart_print_job(job_id):
+    job = models.PrintJob.objects.filter(id=job_id).first()
+    if job is None:
+        logger.warning(f'Attempted to restart non-existent job #{job_id}')
+        return False
+
+    job.state = models.PrintJobState.PENDING.value
+    job.worker = None
+    job.save()
+
+    logger.info(f'Job {job.id}: Restarted')
+    return True

41d54210481edf27eb0670b0118ad0cdc7d60b9d
Amirkeivan Mohtashami <akmohtashami97@gmail.com>
2024-08-26 23:26:22 +0200

    add auto translate menu to the task translation page

--- autotranslate/urls.py
+++ autotranslate/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 
-from django.conf.urls import url
+from django.urls import path
 from .views import *
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^autotranslate_api/', AutoTranslateAPI.as_view(), name='auto_translate_api')
+    path('autotranslate_api/', AutoTranslateAPI.as_view(), name='auto_translate_api')
 ]

1116ce38001a5c2bdaee6a3d9ea617ea2c2f856b
Amirkeivan Mohtashami <akmohtashami97@gmail.com>
2024-08-30 19:15:44 +0200

    Auto: Charge for characters not queries

--- autotranslate/forms.py
+++ autotranslate/forms.py
@@ -4,4 +4,4 @@ from autotranslate import get_supported_languages
 class TranslateRequestForm(forms.Form):
     input_lang = forms.ChoiceField(choices=get_supported_languages())
     output_lang = forms.ChoiceField(choices=get_supported_languages())
-    content = forms.CharField()
+    content = forms.CharField(required=True)

120946ba4d4361da722221687074f37fe7ddadd6
Martin Mares <mj@ucw.cz>
2024-09-01 18:14:34 +0200

    Admin: Better display of autotranslate quotas

--- autotranslate/admin.py
+++ autotranslate/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from autotranslate.models import UserTranslationQuota
 
-# Register your models here.
+
 @admin.register(UserTranslationQuota)
 class UserTranslationQuotaAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'user',
+        'credit',
+        'used',
+    ]
+    ordering = ['user']

4f05a2e6dd332e305aaff8e082dce8444b80e136
Martin Mares <mj@ucw.cz>
2024-09-01 18:14:34 +0200

    Admin: Better display of autotranslate quotas

--- autotranslate/models.py
+++ autotranslate/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserTranslationQuota(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     credit = models.PositiveIntegerField(default=0)
     used = models.PositiveIntegerField(default=0)
-   

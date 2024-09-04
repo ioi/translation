594f76143a804bc631d3cda90c81fcc7c73791ee
Amirkeivan Mohtashami <akmohtashami97@gmail.com>
2024-09-04 17:46:33 +0200

    Show specific error message when source and target language are the same

--- autotranslate/forms.py
+++ autotranslate/forms.py
@@ -5,3 +5,12 @@ class TranslateRequestForm(forms.Form):
     input_lang = forms.ChoiceField(choices=get_supported_languages())
     output_lang = forms.ChoiceField(choices=get_supported_languages())
     content = forms.CharField(required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        input_lang = cleaned_data.get("input_lang")
+        output_lang = cleaned_data.get("output_lang")
+        if input_lang == output_lang:
+            raise forms.ValidationError("Target language must be different from source langauge.")
+        
+        return cleaned_data

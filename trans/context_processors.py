a00e5f348590c55244aefcd73b08fac53ed283d5
Martin Mares <mj@ucw.cz>
2025-07-14 17:57:22 +0200

    Add more items to the top-left menu
    Their visibility is derived from permissions.

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -13,11 +13,22 @@ def ioi_settings(request):
 
 def ioi_user(request):
     user = request.user
-    if user is None:
+    if user is None or not user.is_staff:
         is_editor = False
+        is_staff = False
+        can_notify = False
+    elif user.is_superuser:
+        is_editor = True
+        is_staff = True
+        can_notify = True
     else:
-        is_editor = user.is_staff and user.groups.filter(name='editor').exists() or user.is_superuser
+        is_editor = user.groups.filter(name='editor').exists()
+        # Beware that this is something different from Django's user.is_staff
+        is_staff = user.groups.filter(name='staff').exists()
+        can_notify = user.has_perm('trans.send_notifications')
 
     return {
+        'can_send_notifications': can_notify,
         'is_editor': is_editor,
+        'is_staff': is_staff,
     }
